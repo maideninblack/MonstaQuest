e45507f2ad1b66ce8b32c55e2c5418dd04baef6a
Fu <fu.ripley@gmail.com>
2018-03-07 18:51:15 +0100

    working on MonsterQuestLogic and quest UI

--- Assets/Monster/CleanMonsterQuest.cs
+++ Assets/Monster/CleanMonsterQuest.cs
@@ -8,7 +8,7 @@ public class CleanMonsterQuest : MonoBehaviour
     public const int MAX_SELECTED_CHORDS = 5;
     public const int MAX_TESTS = 3;
 
-    public int testNumber;
+    public int testNumber; // Variable que se randomizará para después generar el test
 
     public int playerScore;
 
@@ -16,7 +16,9 @@ public class CleanMonsterQuest : MonoBehaviour
 
     public Dictionary<int, Chord> chordsSelection; // Diccionario de chords seleccionados, con su key (int)
 
-    bool canValidateTest = false;
+    public bool doTest;
+
+    bool canValidateTest = false; // To trigger ValidateTest() method
 
     List<int> doneTests;
 
@@ -34,6 +36,10 @@ public class CleanMonsterQuest : MonoBehaviour
     {
         playerScore = 0;
 
+        testNumber = 0; // Nunca funcionará valiendo 0 (ha de ser 1-6 que se lo daré con Random.Range)
+
+        doTest = false;
+
         chords = new List<Chord>();
 
         chordsSelection = new Dictionary<int, Chord>();
@@ -45,14 +51,54 @@ public class CleanMonsterQuest : MonoBehaviour
 
 	void Update ()
     {
-		
+		if (doTest)
+        {
+            Debug.Log("Checking if test is already done!");
+            bool newTest = false;
+        }
 	}
 
-    void TestGenerator(int testBumber)
+    void TestGenerator(int testNumber)
     {
         // Array de ints variable que usaré para machacarlo con los scores de los chords del tipo de test que toque
         int[] defaultTest = new int[MAX_CHORDS];
 
-        // Switch de cases de testNumber: un case por cada número de test 
+        testNumber = Random.Range(1, 6);
+
+        // Switch de cases de testNumber: un case por cada número de test (es decir, por cada iteración)
+        switch (testNumber)
+        {
+            case 1:
+                defaultTest = melancholicTest;
+                break;
+            case 2:
+                defaultTest = naughtyTest;
+                break;
+            case 3:
+                defaultTest = tenderTest;
+                break;
+            case 4:
+                defaultTest = cheerfulTest;
+                break;
+            case 5:
+                defaultTest = bizarreTest;
+                break;
+            case 6:
+                defaultTest = sadTest;
+                break;
+        }
+
+        // Los scores de cada chord (20) se cambian a los del tipo de test que haya tocado en el switch
+        for (int i = 0; i < MAX_CHORDS; i++)
+        {
+            chords[i].score = defaultTest[i];
+        }
+
+        doTest = true;
+    }
+
+    private void Reset()
+    {
+        testNumber = 0;
     }
 }
