a5c98c86143a8e3f97abe8ec37bcdd6cba453d8e
Paquito-San <elpacomaster@gmail.com>
2018-03-27 21:45:12 +0200

    Added intro for the music quests

--- Assets/Scripts/Dialogue/Dialogue_Manager.cs
+++ Assets/Scripts/Dialogue/Dialogue_Manager.cs
@@ -2,49 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Dialogue_Manager : MonoBehaviour {
-
-    //public Text nameText;
+    
     public Text dialogueText;
-
-    //public Animator animator;
 
     private Queue<string> sentences; // (FIFO collection, first in first out) Variable that keeps track of all the sentences in our dialogue
 
     public bool justEndDialogue = false;
     
-    //Script_HideCursor cursor;
-
-
-   // public CinemachineBehaviour cineMachine;
-    //public Animator cineMachineAnim;
-
-
-    
-
-	// Use this for initialization
+  // Use this for initialization
 	void Start () {
         sentences = new Queue<string>(); // Initialize variable
         Debug.Log("sentences initialized");
-        //cursor = GetComponent<Script_HideCursor>();
+        
 	}
 	
 	
 
     public void StartDialogue (Dialogue dialogue)
-    {
-        //cineMachineAnim.SetBool("IsActive", true);
-        //cineMachine.Play();
-        
-        //cursor.ShowCursor();
-        
-        //animator.SetBool("IsOpen", true);
-
-        //Debug.Log("Starting conversation with" + dialogue.name);
-
-        //nameText.text = dialogue.name;
-
+    {      
         sentences.Clear();
         Debug.Log("Cleared");
 
@@ -83,7 +61,7 @@ public class Dialogue_Manager : MonoBehaviour {
 
     IEnumerator TypeSentence (string sentence)
     {
-       // dialogueText.text = "";
+        dialogueText.text = "";
         foreach(char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
@@ -95,12 +73,24 @@ public class Dialogue_Manager : MonoBehaviour {
     {
         dialogueText.text = null;
         justEndDialogue = true;
-        //cursor.HideCursor();
+        
         
         Debug.Log("End of conversation");
-       // animator.SetBool("IsOpen", false);
+    }
 
-        //cineMachineAnim.SetBool("IsActive", false);
-
+    public void ButtonNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            SceneManager.LoadScene(3);
+            return;
+        }
+        else justEndDialogue = false;
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+        //dialogueText.text = sentence; this way the text appears instantly
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
     }
 }
